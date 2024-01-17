0de72e92e02cdbf06d8cc0d21da7348601fe1a77
DAM <DAM@DAM-09>
2024-01-17 10:26:59 +0100

    Primer registro de todo

---
+++ src/ejerciciosstring/Ejercicio1.java
@@ -0,0 +1,36 @@
+package ejerciciosstring;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static boolean esVocal( char car) {
+		boolean esVocal=false;
+
+		return esVocal;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String cadena="";
+		int numVocales=0;
+
+		System.out.println("Por favor introduce la cadena de texto");
+		cadena=sc.nextLine();
+		for(int i=0; i<cadena.length();i++) {
+			if (esVocal(cadena.charAt(i)))
+				numVocales++;
+		}
+
+		System.out.println("El número de vocales es: " + numVocales);
+
+
+
+
+
+
+
+
+	}
+
+}
